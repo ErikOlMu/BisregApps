86e42518a609055939f1b6c731f93a5f106ee146
Erik Olivares <erik-2000@hotmail.es>
2022-05-17 11:09:36 +0200

    Programa Etiquetas Finalizado

--- EtiquetasBisreg/MainWindow.xaml.cs
+++ EtiquetasBisreg/MainWindow.xaml.cs
@@ -5,15 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BisregApi.Utilidades;
+using iText.Layout.Properties;
+using iText;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout.Element;
+using iText.Kernel.Pdf.Canvas;
+using System.IO;
+using System.Drawing.Printing;
 
 namespace EtiquetasBisreg
 {
@@ -31,30 +37,62 @@ namespace EtiquetasBisreg
         private void ImportarCSV()
         {
             string file = Environment.GetCommandLineArgs()[1];
-          
-                if (file.EndsWith(".csv"))
+            string temp = System.IO.Path.GetFileNameWithoutExtension(file);
+            PdfWriter writer = new PdfWriter(temp);
+            PdfDocument pdfDoc = new PdfDocument(writer);
+
+            if (file.EndsWith(".csv"))
+            {
+                DataTable data = CSV.GetDataTable(file, true);
+                foreach (DataRow dtRow in data.Rows)
                 {
-                    DataTable data = CSV.GetDataTable(file, true);
-                    foreach (DataRow dtRow in data.Rows)
+                    
+                    string Referencia = (dtRow[data.Columns[1]].ToString() ?? "").Replace("\"", "");
+                    int Copias = int.Parse((dtRow[data.Columns[0]].ToString() ?? "").Replace("\"", ""));
+
+                    Copias = Copias / FindCopias(Referencia);
+                    for (int i = 0; 1 < Copias; i++)
                     {
-                        try
-                        {
-                            string Referencia = (dtRow[data.Columns[1]].ToString() ?? "").Replace("\"", "");
-                            int Copias = int.Parse((dtRow[data.Columns[0]].ToString() ?? "").Replace("\"", ""));
-                            MessageBox.Show(Referencia);
-                        }
-                        catch
-                        {
-                           // LogWrite("Error de Lectura en linea " + data.Rows.IndexOf(dtRow) + "");
-                        }
+                        BuclePage(pdfDoc.AddNewPage(new PageSize(113.0f, 57.0f)), Referencia);
                     }
-                }
-                else
-                {
-                   MessageBox.Show("El Archivo '" + file + "' no es un '.csv'");
+
                 }
 
+
+                pdfDoc.Close();
+                PrintObject.PrintPdf(temp, new PrinterSettings().PrinterName);
+
+            }
+
+
+            else
+            {
+                MessageBox.Show("El Archivo '" + file + "' no es un '.csv'");
+            }
+        }
+
+
+
+        //Diseño etiqueta
+        private static void BuclePage(PdfPage pagina, string content)
+        {
             
+                iText.Layout.Canvas canvas = new iText.Layout.Canvas(pagina, new iText.Kernel.Geom.Rectangle(0, -12, 113.0f, 57.0f));
+                Paragraph p = new Paragraph();
+                p.SetFontSize(11);
+                p.SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER);
+                p.SetVerticalAlignment(iText.Layout.Properties.VerticalAlignment.MIDDLE);
+                p.Add(content);
+
+                canvas.Add(p);
+
+            canvas.Close();
+
+        }
+
+        private static int FindCopias(string Referencia)
+        {
+            return 1;
         }
     }
 }
---
+++ EtiquetasBisreg/PrintObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace EtiquetasBisreg
+{
+    public class PrintObject
+    {
+        public static bool PrintPdf(string file, string printer)
+        {
+            try
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = Registry.LocalMachine.OpenSubKey(
+                        @"SOFTWARE\Microsoft\Windows\CurrentVersion" +
+                        @"\App Paths\AcroRd32.exe").GetValue("").ToString();
+                info.Arguments = string.Format("/h /t \"{0}\" \"{1}\"", file, printer);
+                info.CreateNoWindow = true;
+                info.WindowStyle = ProcessWindowStyle.Hidden;
+
+                Process p = new Process();
+                p.StartInfo = info;
+                p.Start();
+                p.WaitForInputIdle();
+                System.Threading.Thread.Sleep(3000);
+                if (false == p.CloseMainWindow())
+                    p.Kill();
+
+                return true;
+                File.Delete(file);
+            }
+            catch { }
+            return false;
+        }
+       
+    } 
+}
